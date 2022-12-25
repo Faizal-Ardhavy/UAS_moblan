654431eedf581c6ca2008aef4d7655f88533c153
Faizal-Ardhavy <79374894+Faizal-Ardhavy@users.noreply.github.com>
2022-12-25 22:25:10 +0700

    shared preference

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,8 +3,15 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:sizer/sizer.dart';
 import 'package:task_management/core/res/color.dart';
 import 'package:task_management/core/routes/routes.dart';
+import 'package:task_management/pages/nampilinAPI.dart';
+import 'package:task_management/pages/nampilinDiklat.dart';
+import 'package:task_management/pages/nampilinGelar.dart';
+import 'package:task_management/pages/nampilinPenelitian.dart';
+import 'package:task_management/pages/onboarding.dart';
+import 'package:task_management/pages/today_task.dart';
 import 'package:task_management/widgets/circle_gradient_icon.dart';
 import 'package:task_management/widgets/task_group.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,9 +21,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const String prefSelectedIndexKey = 'selectedIndex';
   DateTime now = DateTime.now();
   late DateTime date = DateTime(now.year, now.month, now.day);
   late String dateFormat = date.toString().replaceAll("00:00:00.000", "");
+  @override
+  void initState() {
+    super.initState();
+    getCurrentIndex();
+  }
+
+  void getCurrentIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey(prefSelectedIndexKey)) {
+      setState(() {
+        final index = prefs.getInt(prefSelectedIndexKey);
+        if (index == 1) {
+          print('1');
+            Navigator.pushNamed(context, Routes.todaysTask);
+        } else if (index == 2) {
+          print("2");
+            Navigator.pushNamed(context, Routes.api);
+        } else if (index == 3) {
+          print("3");
+            Navigator.pushNamed(context, Routes.diklat);
+        } else if (index == 4) {
+          print("4");
+            Navigator.pushNamed(context, Routes.penelitian);
+        } else if (index == 5) {
+          print("5");
+            Navigator.pushNamed(context, Routes.gelar);
+        }
+         if (index != null) {
+          print("not null");
+          _selectedIndex = index;
+        }
+      });
+    }
+  }
+
+  void saveCurrentIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt(prefSelectedIndexKey, _selectedIndex);
+    print(_selectedIndex);
+  }
+
+  @override
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +85,10 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: CircleGradientIcon(
               onTap: () {
+                setState(() {
+                  _selectedIndex = 1;
+                });
+                saveCurrentIndex();
                 Navigator.pushNamed(context, Routes.todaysTask);
               },
               icon: Icons.calendar_month,
@@ -123,102 +178,127 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         GestureDetector(
             child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.pink,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              margin: EdgeInsets.all(10.0),
-              width: 58.0,
-              height: 150.0,
-              child:Column(children: const [
-                Align(alignment: Alignment.bottomCenter),
-                Icon(Icons.house, size: 50, color: Colors.pink),
-                Text('Kumpulan Jurusan',textAlign: TextAlign.center, style: TextStyle(
-                  fontSize: 25,
-                  fontFamily: "Raleway",
-                  color: Colors.pink
-                ),)
-
-              ],) 
-              
-            ),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.pink,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                margin: EdgeInsets.all(10.0),
+                width: 58.0,
+                height: 150.0,
+                child: Column(
+                  children: const [
+                    Align(alignment: Alignment.bottomCenter),
+                    Icon(Icons.house, size: 50, color: Colors.pink),
+                    Text(
+                      'Kumpulan Jurusan',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: "Raleway",
+                          color: Colors.pink),
+                    )
+                  ],
+                )),
             onTap: () {
+              setState(() {
+                _selectedIndex = 2;
+              });
+              saveCurrentIndex();
+
               Navigator.pushNamed(context, Routes.api);
             }),
-            GestureDetector(
+        GestureDetector(
             child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.orange,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              margin: EdgeInsets.all(10.0),
-              width: 58.0,
-              height: 150.0,
-              child:Column(children: const [
-                Align(alignment: Alignment.bottomCenter),
-                Icon(Icons.archive, size: 50, color: Colors.orange),
-                Text('Kumpulan Diklat',textAlign: TextAlign.center, style: TextStyle(
-                  fontSize: 25,
-                  fontFamily: "Raleway",
-                  color: Colors.orange
-                ),)
-
-              ],) 
-              
-            ),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.orange,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                margin: EdgeInsets.all(10.0),
+                width: 58.0,
+                height: 150.0,
+                child: Column(
+                  children: const [
+                    Align(alignment: Alignment.bottomCenter),
+                    Icon(Icons.archive, size: 50, color: Colors.orange),
+                    Text(
+                      'Kumpulan Diklat',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: "Raleway",
+                          color: Colors.orange),
+                    )
+                  ],
+                )),
             onTap: () {
+              setState(() {
+                _selectedIndex = 3;
+              });
+              saveCurrentIndex();
               Navigator.pushNamed(context, Routes.diklat);
             }),
-             GestureDetector(
+        GestureDetector(
             child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.green,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              margin: EdgeInsets.all(10.0),
-              width: 58.0,
-              height: 150.0,
-              child:Column(children: const [
-                Align(alignment: Alignment.bottomCenter),
-                Icon(Icons.science, size: 50, color: Colors.green),
-                Text('Kumpulan Penelitian',textAlign: TextAlign.center, style: TextStyle(
-                  fontSize: 25,
-                  fontFamily: "Raleway",
-                  color: Colors.green
-                ),)
-
-              ],) 
-              
-            ),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.green,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                margin: EdgeInsets.all(10.0),
+                width: 58.0,
+                height: 150.0,
+                child: Column(
+                  children: const [
+                    Align(alignment: Alignment.bottomCenter),
+                    Icon(Icons.science, size: 50, color: Colors.green),
+                    Text(
+                      'Kumpulan Penelitian',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: "Raleway",
+                          color: Colors.green),
+                    )
+                  ],
+                )),
             onTap: () {
+              setState(() {
+                _selectedIndex = 4;
+              });
+              saveCurrentIndex();
               Navigator.pushNamed(context, Routes.penelitian);
             }),
         GestureDetector(
             child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.blue,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              margin: EdgeInsets.all(10.0),
-              width: 58.0,
-              height: 150.0,
-              child:Column(children: const [
-                Align(alignment: Alignment.bottomCenter),
-                Icon(Icons.school, size: 50, color: Colors.blue),
-                Text('Kumpulan Gelar Akademik',textAlign: TextAlign.center, style: TextStyle(
-                  fontSize: 25,
-                  fontFamily: "Raleway",
-                  color: Colors.blue
-                ),)
-
-              ],) 
-              
-            ),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.blue,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                margin: EdgeInsets.all(10.0),
+                width: 58.0,
+                height: 150.0,
+                child: Column(
+                  children: const [
+                    Align(alignment: Alignment.bottomCenter),
+                    Icon(Icons.school, size: 50, color: Colors.blue),
+                    Text(
+                      'Kumpulan Gelar Akademik',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: "Raleway",
+                          color: Colors.blue),
+                    )
+                  ],
+                )),
             onTap: () {
+              setState(() {
+                _selectedIndex = 5;
+              });
+              saveCurrentIndex();
               Navigator.pushNamed(context, Routes.gelar);
             }),
       ],
